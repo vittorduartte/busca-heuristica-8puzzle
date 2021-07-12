368de8c1bc12e62d8c743c07ea52488ce6fbc654
Mateus Vitor <mateusriograndense@gmail.com>
2021-07-12 20:30:08 -0300

    :sparkles: Correção das condições das regras da mecânica do jogo. Implementação da função de captura do estado atual.

--- script.js
+++ script.js
@@ -1,20 +1,43 @@
-$(document).ready(() => {
+function stateNow() {
+    var stateNow = $(".board__element");
+    var board = [];
+    var gap_localization = 0;
+    stateNow.map((index, item) => {
+        if(item.innerHTML.trim()) {
+            board.push(item.innerHTML.trim());
+        } else {
+            board.push('gap');
+            gap_localization = index;
+        }   
+    })
+    return {
+        board,
+        gap_localization
+    }
+}
 
-    $(".board__element:not(.is-gap)").on("click", (event) => {
-        var gapLocalization = $(".board__element.is-gap").attr('id');
+$(function () {
+
+    $(document).on("click", ".board__element:not(.is-gap)", function (event) {
+        var gapLocalization = $(".board__element.is-gap").attr("id").split('x');
         var target = $(event.target);
-        var targetLocalization = target.attr('id');
+        var targetLocalization = target.attr("id").split('x');
+        
+        if (Number(targetLocalization[1]) == Number(gapLocalization[1]) + 1 && targetLocalization[0] == gapLocalization[0] ||
+            Number(targetLocalization[1]) == Number(gapLocalization[1]) - 1 && targetLocalization[0] == gapLocalization[0] ||
+            Number(targetLocalization[0]) == Number(gapLocalization[0]) + 1 && targetLocalization[1] == gapLocalization[1] ||
+            Number(targetLocalization[0]) == Number(gapLocalization[0]) - 1 && targetLocalization[1] == gapLocalization[1]) {
 
-        if ((Number(targetLocalization)).toFixed(1) == (Number(gapLocalization) + 0.1).toFixed(1) ||
-            (Number(targetLocalization)).toFixed(1) == (Number(gapLocalization) - 0.1).toFixed(1) ||
-            (Number(targetLocalization)).toFixed(1) == (Number(gapLocalization) + 1).toFixed(1) ||
-            (Number(targetLocalization)).toFixed(1) == (Number(gapLocalization) - 1).toFixed(1)) 
-        {
             var targetValue = target.contents().text().trim();
+            target.text('');
 
-            $(".board__element.is-selected").removeClass('is-selected');
-            target.addClass("is-selected");
+            $(`#${gapLocalization[0]}x${gapLocalization[1]}`).text(targetValue);
+
+            $(".board__element.is-gap").removeClass("is-gap");
+            target.addClass("is-gap");
         }
+        
+        console.log(stateNow());
     });
 
 });
